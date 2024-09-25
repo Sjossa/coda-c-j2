2c4e8a86d53a7219a98afa6b628c4dfea9c2a052
Sjossa <johnny.sass2001@gmail.com>
2024-09-25 14:11:09 +0200

    calcul

---
+++ ex-4/main.c
@@ -0,0 +1,9 @@
+#include <stdlib.h>
+#include <stdio.h>
+int main (){
+int paire = 10;
+int inpaire = 45;
+int resultat = paire * inpaire;
+printf("%d x %d = %d\n", paire, inpaire, resultat);
+exit(0);
+}
