b1c09f7325d2d4bfc14d862ee7a93487876ec28c
Sjossa <johnny.sass2001@gmail.com>
2024-09-25 14:26:18 +0200

    moyenne

---
+++ ex-5/main.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include <stdio.h>
+	int main(){
+		double calcul = 12 + 7;
+
+		double moyenne = 2;
+
+		double resultat = calcul/moyenne;
+		printf("La moyenne de 12 et 7 est égale à %.2f\n",resultat);
+		
+		exit(0);
+		
+
+	}	
